6e073e60c7b3cb3603b3d0c94b2dac257058bf4f
Amrutha956 <amrutharamesh465@gmail.com>
2024-05-05 13:19:45 +0530

    cart and checkout page

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from store.models import Product
+from store.models import Product,Variation
 from accounts.models import Account
 
 # Create your models here.
@@ -13,6 +13,7 @@ class Cart(models.Model):
     
 class CartItem(models.Model):
     user = models.ForeignKey(Account,on_delete=models.CASCADE,null=True)
+    variations =models.ManyToManyField(Variation,blank=True)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart,on_delete=models.CASCADE,null=True)
     quantity = models.IntegerField()
@@ -22,7 +23,7 @@ class CartItem(models.Model):
         return self.product.price * self.quantity
     
 
-    def __str__(self):
+    def __unicode__(self):
         return self.product
 
 
